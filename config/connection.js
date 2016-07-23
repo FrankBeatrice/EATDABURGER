68a95b2aab5e8e9bcf2d344113925c5a5fd8f0c9
FrankBeatrice <Frankie@Franks-MacBook-Pro.local>
2016-07-23 15:09:26 -0400

    fix bug

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,19 @@
 var mysql = require('mysql');
 var connection;
+i
+f(process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-if (process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'ky1kwezqps0a6i2e',
-        password: 'p515f9x3d60voi2y',
-        database: 'a4pyhb1wup32mbn',
-    });
-}
+connection=mysql.createConnection({ 
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
 
-
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
 });
+};
 
+
+connection.connect();
 module.exports = connection;
