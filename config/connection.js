f4ed154c43316f0aa737fa9c7c66b97d1ff17a61
FrankBeatrice <Frankie@Franks-MacBook-Pro.local>
2016-07-23 14:43:18 -0400

    Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-});
+var connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'ky1kwezqps0a6i2e',
+        password: 'p515f9x3d60voi2y',
+        database: 'a4pyhb1wup32mbn',
+    });
+}
 
 
 connection.connect(function(err) {
